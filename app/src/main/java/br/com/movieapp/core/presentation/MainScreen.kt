3a535ccb2ae8d54315d342641b923578fed9889e
gabri <gabriel.amoreira@outlook.com>
2024-04-15 10:04:51 -0300

    Fixes Loading and Spacing errors.

--- app/src/main/java/br/com/movieapp/core/presentation/MainScreen.kt
+++ app/src/main/java/br/com/movieapp/core/presentation/MainScreen.kt
@@ -1,8 +1,11 @@
 package br.com.movieapp.core.presentation
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import br.com.movieapp.core.presentation.navigation.BottomNavigationBar
 import br.com.movieapp.core.presentation.navigation.NavigationGraph
@@ -11,12 +14,14 @@ import br.com.movieapp.core.presentation.navigation.NavigationGraph
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen(navController: NavHostController) {
-    Scaffold (
+    Scaffold(
         bottomBar = {
             BottomNavigationBar(navController = navController)
         },
-        content = {
-            NavigationGraph(navController = navController)
+        content = { paddingValues ->
+            Box(modifier = Modifier.padding(paddingValues)) {
+                NavigationGraph(navController = navController)
+            }
         }
     )
 }
