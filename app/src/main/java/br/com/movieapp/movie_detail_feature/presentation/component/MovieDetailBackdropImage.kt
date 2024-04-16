ed35414471b8619e772225007727f5ccf0a4767b
gabri <gabriel.amoreira@outlook.com>
2024-04-16 10:29:44 -0300

    Implements DetailImage Component.

---
+++ app/src/main/java/br/com/movieapp/movie_detail_feature/presentation/component/MovieDetailBackdropImage.kt
@@ -0,0 +1,45 @@
+package br.com.movieapp.movie_detail_feature.presentation.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.movieapp.R
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun MovieDetailBackdropImage(
+    backdropImageUrl: String, modifier: Modifier,
+) {
+    Box(modifier = modifier) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(backdropImageUrl)
+                .crossfade(true)
+                .error(R.drawable.ic_error_image)
+                .placeholder(R.drawable.ic_placeholder)
+                .build(),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun MovieDetailBackdropImagePreview() {
+    MovieDetailBackdropImage(
+        backdropImageUrl = "",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    )
+}
